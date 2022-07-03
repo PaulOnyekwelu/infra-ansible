a1e7955fd9e4fb2b46cae8d30a5895a817453357
Paul Onyekwelu <paul@workongrid.com>
2022-07-03 17:59:50 +0100

    ci for creating aws infrastructure

--- cloudformation/create-infra.sh
+++ cloudformation/create-infra.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-aws cloudformation create-stack \
-  --stack-name Ansible-EC2-Instance \
-  --template-body file://infra.yml \
-  --parameters file://infra-parameters.json \
-  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
+
+# for aws cloudformation create-stack
+# aws cloudformation create-stack \
+#   --stack-name Ansible-EC2-Instance \
+#   --template-body file://infra.yml \
+#   --parameters file://infra-parameters.json \
+#   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
+
+# for aws cloudformation deploy
+aws cloudformation deploy \
+  --stack-name myStack-${CIRCLE_WORKFLOW_ID:0:5} \
+  --template-file infra.yml \
+  --parameter-overrides file://deploy-parameters.json \
+  --region=us-west-2
+
   
